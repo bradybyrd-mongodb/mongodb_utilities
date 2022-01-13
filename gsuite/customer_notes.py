8a62ef0e25e54fd431cd16281b259844bc3cd75e
bradybyrd-mongodb <brady.byrd@mongodb.com>
2022-01-13 15:07:37 -0500

    adding changes for customer_notes and adding mongo_stream

--- gsuite/customer_notes.py
+++ gsuite/customer_notes.py
@@ -4,7 +4,9 @@
 
 import sys
 import os
+import inspect
 import csv
+import copy
 import json
 import datetime
 import random
@@ -14,19 +16,55 @@ import multiprocessing
 import pprint
 import bson
 from bson.objectid import ObjectId
-from ../bbutil import Util
-from datetime import datetime
 from collections import OrderedDict
 from pymongo import MongoClient
+
+# Hack to import from parent directory
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+from bbutil import Util
 
 '''
 #------------------------------------------#
 # Notes
+Read through all dirs in Customers
+Exclude .files
+By Folder:
+    list all files
+    note and ignore subfolder contents
+    _notes.md file, import it
+    any other .md file, import it
+
+Notes File:
+    #------------------------------------------#  <= Border
+    #  Call 10/14/21 - Charts Help   <= Date, subject
+    Data Science team <= People
+
+
+Document Schema:
+{
+    customer: "Agero",
+    folder: "Agero",
+    rep: "Vin D'Amato",
+    status: "active",
+    "brief": "happy peppy customer",
+    quality: "good",
+    contacts: [
+        {name: "Bob White", "position" : "Ent Architect", "notes" : "nice guy"}
+    ],
+    "notes" : [
+        {"date" : "3-21-2021", "subject": "meeting", "body" : "lots of text"}
+    ],
+    "files" : [
+        {"name" : "stuff.json", "path" : "fullpath", created: , "modified" , "size", is_object: True}
+    ]
+}
 #
 '''
 
 settings_file = "customer_notes_settings.json"
-path ="C:/workspace/python"
+path ="Users/"
 jobs = []
 
 def file_walk():
@@ -39,34 +77,63 @@ def file_walk():
     if "path" in ARGS:
         path = ARGS["path"]
     else:
-        print("Send path= argument")
-        sys.exit(1)
+        path = settings["base_document"]["path"]
+        if not path:
+            print("Send path= argument")
+            sys.exit(1)
     main_process = multiprocessing.current_process()
     bb.logit("#------------------------------------------------------------#")
     bb.logit('# Main process is %s %s' % (main_process.name, main_process.pid))
     conn = client_connection()
     db = conn[settings["database"]]
+    doc = OrderedDict()
     cnt = 0
     tot = 0
-
+    company = "base_folder"
+    last_path = path
+    base_parts = path.split("/")
+    base_depth = len(base_parts)
     for root, dirs, files in os.walk(path):
         bulk_docs = []
+        file_arr = []
         cnt = 0
-        bb.logit("#--------------------------------------------------#")
-        bb.logit(f'# {root}')
+        cur_parts = root.split("/")
+        if ".git" in root:
+            continue
+        if len(cur_parts) > (base_depth + 1):
+            # deeper sub, ignore
+            file_arr.append(file_info(root,"dir"))
+            bb.logit(f'# {root} - subdir')
+            continue
+        if root != last_path:
+            bulk_docs.append(doc)
+            doc = OrderedDict()
+            company = cur_parts[-1]
+            last_path = root
+            bb.logit("#--------------------------------------------------#")
+            bb.logit(f'# Firm: {company}')
+            bb.logit(f'# {root}')
+            doc["customer"] = company
+        else:
+            doc["customer"] = company
+        doc["path"] = root
         dir_doc = file_info(root, "dir")
         db[collection].insert_one(dir_doc)
         for fil in files:
+            if fil[0] == ".":
+                continue
             cur_file = os.path.join(root,fil)
             new_doc = (file_info(cur_file))
-            bulk_docs.append(new_doc)
+            file_arr.append(new_doc)
+            if "_notes.md" in fil:
+                process_notes(fil, root)
             bb.logit(cur_file)
             cnt += 1
             if cnt == batch_size:
                 tot += batch_size
                 bb.logit(f'Loading {batch_size} more - total: {tot}')
                 db[collection].insert_many(bulk_docs)
-                update_directories(bulk_docs, root)
+                #update_directories(bulk_docs, root)
                 bulk_docs = []
                 cnt = 0
         # Get leftovers
@@ -74,17 +141,108 @@ def file_walk():
             tot += len(bulk_docs)
             bb.logit(f'Loading {len(bulk_docs)} more - total: {tot}')
             db[collection].insert_many(bulk_docs)
-            update_directories(bulk_docs, root)
+            #update_directories(bulk_docs, root)
 
     for i in jobs:
         i.join()
     bb.logit(f'Completed {tot} directory items')
     conn.close()
 
+def process_notes(name,fullpath):
+    # Break up notes file
+    content = ""
+    with open(os.path.join(fullpath,name)) as f:
+        content = f.readlines()
+    notes = []
+    bb.logit(f'Parsing Notes: {name}')
+    note = ""
+    event_date = "unknown" #datetime.datetime.now()
+    subject = "Generic Meeting"
+    body = ""
+    in_header = -1
+    cnt = 0
+    for line in content:
+        if "#---------------------------------" in line:
+            bb.logit(f'header[{cnt}] - {line}')
+            in_header = 1
+            note = {"date" : event_date, "subject": subject, "body" : body}
+            bb.logit(f'Adding: {note["date"]} - {note["subject"]}')
+            notes.append(copy.deepcopy(note))
+            event_date = "unknown" #datetime.datetime.now()
+            subject = "Generic Meeting"
+            body = ""
+        else:
+            if in_header == 1:
+                if line.startswith("# "):
+                    subject = line
+                else:
+                    in_header = -1
+                res = line_has_date(line)
+                if res != "none":
+                    event_date = res
+                bb.logit(f'subject[{cnt}] {event_date} - {line.strip()}')
+                in_header += 1
+            elif in_header == 2:
+                if line.startswith("# "):
+                    xtra = line
+                    subject = f'{subject.strip()} | {line.strip()}'
+                    in_header += 1
+                else:
+                    body = f'{body}{line}'
+                    in_header = -1
+                res = line_has_date(line)
+                if res != "none":
+                    event_date = res
+                bb.logit(f'xtra[{cnt}] {event_date} - {line.strip()}')
+            elif in_header == 3:
+                if line.startswith("# "):
+                    xtra = line
+                    subject = f'{subject.strip()} | {line.strip()}'
+                in_header = -1
+                res = line_has_date(line)
+                if res != "none":
+                    event_date = res
+                bb.logit(f'xtra[{cnt}] {event_date} - {line.strip()}')
+            else:
+                body += line
+                bb.logit(f'body[{cnt}]')
+        cnt += 1
+    bb.logit("---- Processed ----")
+    for k in notes:
+        print("#---------------------------#")
+        print(k["date"])
+        print(f'Subject: {k["subject"]}')
+        print(f'Body: \n{body}')
+        print(" ")
+
+
+def line_has_date(txt):
+    #fin date string in line
+    ans = re.findall(r"\d+\/\d+\/\d+",txt)
+    month = 1
+    day = 1
+    year = 2010
+    answer = "none"
+    try:
+        if len(ans) > 0:
+            dat = ans[0]
+            parts = dat.split("/")
+            month = int(parts[0])
+            day = int(parts[1])
+            year = int(parts[2])
+            if year < 100:
+                year += 2000
+            answer = datetime.datetime(year,month,day, 11, 59)
+    except Exception as e:
+        bb.logit(f'Error: {str(e)}, orig: {txt}')
+    return(answer)
+
 def file_info(file_obj, type = "file"):
     try:
         file_stats = os.stat(file_obj)
         doc = OrderedDict()
+        paths = file_obj.split("/")
+        doc["name"] = paths[-1]
         doc["path_raw"] = file_obj
         if type == "file":
             doc["is_object"] = True
@@ -98,9 +256,9 @@ def file_info(file_obj, type = "file"):
         doc["owner"] = f'{file_stats.st_uid}:{file_stats.st_gid}'
         m_at = file_stats.st_mtime
         c_at = file_stats.st_ctime
-        doc["modified_at"] = datetime.fromtimestamp(m_at).strftime('%Y-%m-%d %H:%M:%S')
-        doc["created_at"] = datetime.fromtimestamp(c_at).strftime('%Y-%m-%d %H:%M:%S')
-        doc["paths"] = file_obj.split("/")
+        doc["modified_at"] = datetime.datetime.fromtimestamp(m_at).strftime('%Y-%m-%d %H:%M:%S')
+        doc["created_at"] = datetime.datetime.fromtimestamp(c_at).strftime('%Y-%m-%d %H:%M:%S')
+        doc["paths"] = paths
     except:
         bb.logit(f'Path: {file_obj} inaccessible')
         doc = {"error" : True}
@@ -137,5 +295,9 @@ if __name__ == "__main__":
         sys.exit(1)
     elif ARGS["action"] == "walk":
         file_walk()
+    elif ARGS["action"] == "process_notes":
+        path = ARGS["path"]
+        fil = ARGS["filename"]
+        process_notes(fil, path)
     else:
         print(f'{ARGS["action"]} not found')
---
+++ mongo_stream/mongo_stream.py
@@ -0,0 +1,238 @@
+'''
+#---------- Instructions ---------------#
+-- Make sure libraries are installed:
+   pip3 install pymongo dnspython
+-- To Run:
+   python3 mongo_stream.py --f <settings file>
+-- Default settings file name
+   mongo_stream_settings.json
+
+To suppress SSL Verify add the follwoing into a connection string: &ssl_cert_reqs=CERT_NONE
+
+Sample Output:
+#     Scanning Changes for cluster:  clinical
+{'_id': {'_data': '8261BA3DB0000000012B022C0100296E5A1004175D21F922A34BD1BDD3E00939C3B23946645F6964006461BA3D876D35EFACFD55E8A60004'}, 'operationType': 'insert', 'clusterTime': Timestamp(1639595440, 1), 'fullDocument': {'_id': ObjectId('61ba3d876d35efacfd55e8a6'), 'pos': '86675', 'chrom': 'chr3'}, 'ns': {'db': 'clinical', 'coll': 'results'}, 'documentKey': {'_id': ObjectId('61ba3d876d35efacfd55e8a6')}}
+'''
+
+from io import BufferedIOBase
+from pymongo import MongoClient
+import os
+import sys
+import argparse
+import json
+from time import time
+from datetime import datetime
+
+class Util:
+   def __init__(self, details = {}):
+       self.hfile = False
+
+   def logit(self, message, log_type = "INFO", display_only = True):
+       cur_date = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+       stamp = f"{cur_date}|{log_type}> "
+       if type(message) == dict or type(message) == list:
+           message = str(message)
+       if log_type == "raw":
+           message = "Raw output, check file"
+       for line in message.splitlines():
+           print(f"{stamp}{line}")
+
+   def message_box(self, msg, mtype = "sep"):
+       tot = 100
+       start = ""
+       res = ""
+       msg = msg[0:84] if len(msg) > 85 else msg
+       ilen = tot - len(msg)
+       if (mtype == "sep"):
+           start = f'#{"-" * int(ilen/2)} {msg}'
+           res = f'{start} {"-" * (tot - len(start) + 1)}#'
+       else:
+           res = f'#{"-" * tot}#\n'
+           start = f'#{" " * int(ilen/2)} {msg} '
+           res += f'{start}{" " * (tot - len(start) + 1)}#\n'
+           res += f'#{"-" * tot}#\n'
+       self.logit(res)
+       return res
+
+   def read_json(self, json_file, is_path = True):
+       result = {}
+       if is_path:
+           with open(json_file) as jsonfile:
+               result = json.load(jsonfile)
+       else:
+           result = json.loads(json_file)
+       return result
+
+def watchCluster(streamCon, destCon, settings):
+   # we can specify operation types as well as other conditions here
+   pipeline = [{'$match': {'operationType': {'$in': ['update', 'insert', 'replace', 'delete']}}}]
+
+   loggingDB = settings["loggingDB"]
+   loggongColl = settings["logCollection"]
+   tokenCollection = settings["tokenCollection"]
+   cluster = settings["cluster"]
+   exclusion_list = settings["exclusionList"]
+
+   bb.message_box(f"Scanning Changes for cluster:  {cluster}", "title")
+   try:
+       # Set the resume token name to cluster.db.collection
+       tokenId = cluster
+
+       # resume token is how we pick up where we left off on restart.
+       # In this example, we're also storing it in the dest cluster.
+       resumeToken = None
+       # fetch last resume token - may not be present in which case we start from the beginning
+       rt = destCon[loggingDB][tokenCollection].find_one({"_id": tokenId})
+       if rt is None:
+           # create token entry
+           destCon[loggingDB][tokenCollection].insert_one({"_id": tokenId, 'value': None})
+       else:
+           resumeToken = rt['value']
+
+       # start the watch process - note the 'updateLookup' option here
+       # which returns the full document to the watch consumer (this program)
+       with streamCon.watch(pipeline, full_document='updateLookup', resume_after=resumeToken, batch_size=settings["batchSize"]) as stream:
+           cnt = 1
+           last_ts = 0
+           print_time = time()
+           while stream.alive:
+               u = stream.try_next()
+               resume_token = stream.resume_token
+
+               if u is not None:
+                   if cnt == 0 or (time() - print_time >= 1):
+                       v = streamCon["local"]["oplog.rs"].find({}, {"_id":0,"ts":1}).sort([("$natural",-1)]).limit(1).next()
+                       last_ts = u["clusterTime"].time
+                       millis = v["ts"].time-last_ts
+                   db = u['ns']['db']
+                   coll = u['ns']['coll']
+                   if db + '.' + coll not in exclusion_list:
+                       cnt = cnt + 1
+                       if u['operationType'] in ['insert', 'update', 'replace']:
+                           # Upsert doc dest
+                           if u['fullDocument'] is not None:
+                               destCon[db][coll].replace_one({'_id': u['fullDocument']['_id']}, u['fullDocument'], upsert=True)
+                           else:
+                               u['fullDocument'] = "ERROR Missing document"
+
+                       elif u['operationType'] == 'delete':
+                           # receive the original document
+                           full_doc = destCon[db][coll].find_one(u['documentKey'])
+                           if full_doc == None:
+                               full_doc = u['documentKey']
+                           u["fullDocument"] = full_doc
+
+                           # remove from doc dest
+                           destCon[db][coll].delete_one(u['documentKey'])
+
+                       if settings["enableLogging"] == "Yes":
+                           destCon[loggingDB][loggongColl].insert_one({"timestamp" : datetime.now(), "op" : u['operationType'], "collection" : coll, "document" : u['fullDocument']})
+
+                       # just to show the changestream object - comment out for less noise
+                       # print(f'Modifying: {coll}, op: {u["operationType"]}, Id: {u["fullDocument"]["_id"]}')
+               else:
+                   if cnt > 0:
+                       millis = 0
+                       cnt = 0
+                       bb.message_box("All changes applied waiting for the next block of changes")
+
+               # Update resume token
+               destCon[loggingDB][tokenCollection].update_one({"_id": tokenId}, {'$set': {'value': resume_token}})
+               if (time() - print_time >= 10):
+                   bb.message_box(f"Replication lag is {millis} milliseconds")
+                   print_time = time()
+   except Exception as err:
+       exc_type, exc_obj, exc_tb = sys.exc_info()
+       fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+       print(f'ERROR: {exc_type} in {fname} on line {exc_tb.tb_lineno}')
+       print(err)
+       if u is not None:
+           print("--offending oplog--")
+           print(u)
+
+def run_tester(settings):
+   batchSize = settings["batchSize"]
+   cluster = settings["cluster"]
+
+   bb.message_box(f"Scanning Changes for cluster:  {cluster}", "title")
+
+   pipeline = [{'$match': {'operationType': {'$in': ['update', 'insert', 'replace', 'delete']}}}]
+   resumeToken = None
+
+   try:
+       streamCon = MongoClient(
+           host=settings["source_uri"],
+           serverSelectionTimeoutMS=2000,
+           socketTimeoutMS=500,
+           connectTimeoutMS=500
+       )
+
+       with streamCon.watch(pipeline, full_document='updateLookup', resume_after=resumeToken, batch_size=batchSize) as stream:
+           for u in stream:
+               print(f'Modifying: {u["ns"]["coll"]}, op: {u["operationType"]}')
+               print(u)
+
+       streamCon.close()
+
+   except Exception as err:
+       print('Error occurred:' + str(err))
+       sys.exit(1)
+
+def run(settings):
+   try:
+       streamCon = MongoClient(
+           host=settings["source_uri"],
+           serverSelectionTimeoutMS=settings["serverSelectionTimeoutMS"],
+           socketTimeoutMS=settings["socketTimeoutMS"],
+           connectTimeoutMS=settings["connectTimeoutMS"]
+       )
+
+       destCon = MongoClient(
+           host=settings["dest_uri"],
+           serverSelectionTimeoutMS=settings["serverSelectionTimeoutMS"],
+           socketTimeoutMS=settings["socketTimeoutMS"],
+           connectTimeoutMS=settings["connectTimeoutMS"]
+       )
+
+       watchCluster(streamCon, destCon, settings)
+
+       streamCon.close()
+       destCon.close()
+
+   except Exception as err:
+       print('Error occurred:' + str(err))
+       sys.exit(1)
+
+def init():
+   parser = argparse.ArgumentParser(description="MongoStream")
+   parser.add_argument('-f', '--file', type=str, help="Settings file", required=False, default="mongo_stream_settings.json")
+   args = parser.parse_args()
+   settings_file = args.file
+
+   print("\n")
+   bb.message_box("Using settings file {settings_file}")
+
+   settings = bb.read_json(settings_file)
+
+   if settings["exclusionList"] == None:
+       exclusion_list = []
+   else:
+       exclusion_list = [s.strip() for s in settings["exclusionList"].split(",")]
+   exclusion_list.append(settings["loggingDB"] + '.' + settings["tokenCollection"])
+   exclusion_list.append(settings["loggingDB"] + '.' + settings["logCollection"])
+   settings["exclusionList"] = exclusion_list
+
+   if settings["batchSize"] < 100 or settings["batchSize"] > 1000:
+       settings["batchSize"] = 1000
+   print(settings)
+   print("\n")
+
+   return settings
+
+if __name__ == "__main__":
+   global bb
+   bb = Util()
+
+   settings = init()
+   #run_tester(settings)
+   run(settings)
