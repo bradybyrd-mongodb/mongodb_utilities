ee397fefdb3a87d6781caf38ac6b0de34f4cd5a0
yberrada <younesberrada1@gmail.com>
2022-09-09 11:15:21 -0400

    pushes messages to 2 topics

--- comm_tracker/commtracker.py
+++ comm_tracker/commtracker.py
@@ -1,3 +1,7 @@
+from message_loader import MessageLoader
+from mongo_loader import DbLoader
+from id_generator import Id_generator
+from bbutil import Util
 import sys
 import os
 import csv
@@ -29,14 +33,11 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(base_dir))
 sys.path.append(os.path.join(base_dir, "templates"))
 #from t_comm import CommFactory
-from bbutil import Util
-from id_generator import Id_generator
-from mongo_loader import DbLoader
-from message_loader import MessageLoader
 
 fake = Faker()
-letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-providers = ["cigna","aetna","anthem","bscbsma","kaiser"]
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+           "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+providers = ["cigna", "aetna", "anthem", "bscbsma", "kaiser"]
 
 '''
  #  CommTracker
@@ -72,6 +73,7 @@ Communications Cache from Hadoop UDF
 '''
 settings_file = "commtracker_settings.json"
 
+
 def load_messages():
     # read settings and echo back
     bb.message_box("Activity Loader", "title")
@@ -82,7 +84,7 @@ def load_messages():
     inc = 0
     multiprocessing.set_start_method("fork", force=True)
     for item in range(num_procs):
-        p = multiprocessing.Process(target=worker_load, args = (item,))
+        p = multiprocessing.Process(target=worker_load, args=(item,))
         jobs.append(p)
         p.start()
         time.sleep(1)
@@ -92,6 +94,7 @@ def load_messages():
     bb.logit('Main process is %s %s' % (main_process.name, main_process.pid))
     for i in jobs:
         i.join()
+
 
 def worker_load(ipos):
     #  Reads file and finds values
@@ -113,8 +116,8 @@ def worker_load(ipos):
         worker_profile_load(feed)
     else:
         worker_message_generate(feed)
-    #worker_claim_load(pgcon,tables)
-    #worker_rx_load(pgcon,tables)
+    # worker_claim_load(pgcon,tables)
+    # worker_rx_load(pgcon,tables)
     end_time = datetime.datetime.now()
     time_diff = (end_time - start_time)
     execution_time = time_diff.total_seconds()
@@ -124,7 +127,9 @@ def worker_load(ipos):
 # --------------------------------------------------------- #
 #  Pub Sub Publish
 # --------------------------------------------------------- #
-def message_publisher(stream):
+
+
+def message_publisher(stream, topics):
     # read settings and echo back
     bb.message_box("Pub/Sub Message Subscriiption", "title")
     bb.logit(f'# Settings from: {settings_file}')
@@ -133,12 +138,15 @@ def message_publisher(stream):
     jobs = []
     inc = 0
     multiprocessing.set_start_method("fork", force=True)
-    for item in range(num_procs):
-        p = multiprocessing.Process(target=worker_message_generate, args = (item, stream))
-        jobs.append(p)
-        p.start()
-        time.sleep(1)
-        inc += 1
+    for topic_id in range(len(topics)):
+        for item in range(num_procs):
+            p = multiprocessing.Process(
+                target=worker_message_generate, args=(item, stream, topics[topic_id]))
+            jobs.append(p)
+            p.start()
+
+            time.sleep(1)
+            inc += 1
 
     main_process = multiprocessing.current_process()
     bb.logit('Main process is %s %s' % (main_process.name, main_process.pid))
@@ -146,7 +154,7 @@ def message_publisher(stream):
         i.join()
 
 
-def worker_message_generate(proc_num, stream):
+def worker_message_generate(proc_num, stream, topic):
     '''
         generate messages - publish to pub/sub
         accumulate x-million
@@ -156,21 +164,21 @@ def worker_message_generate(proc_num, stream):
 
         single-instance now
         gke - scale up
-    
+
     '''
     #  Send a copy of the claim with one or two field changes
     #  Add an updateDate and sequencenumber, updateName = TCD-1
     cur_process = multiprocessing.current_process()
     prefix = "COMT"
     feed = False
-    loader = MessageLoader("kafka-topic",{"settings": settings})
+    loader = MessageLoader(topic, {"settings": settings})
     interval = 0
     base_counter = settings["base_counter"]
     batch_size = settings["batch_size"]
     num_records = settings["num_records"]
     summary_template = settings["summary_template"]
     num_iterations = int(num_records/batch_size)
-    IDGEN.set({"seed" : base_counter, "size" : num_records, "prefix" : prefix})
+    IDGEN.set({"seed": base_counter, "size": num_records, "prefix": prefix})
     if "feed" in ARGS:
         sample_size = 10
         num_iterations = int(ARGS["feed"])
@@ -187,7 +195,7 @@ def worker_message_generate(proc_num, stream):
     for k in range(num_iterations):
         #bb.logit(f'Iter: {k} of {num_iterations}, {batch_size} per batch - total: {icnt}')
         icnt = 0
-        base_id = int(IDGEN.get(prefix, num_records).replace(prefix,""))
+        base_id = int(IDGEN.get(prefix, num_records).replace(prefix, ""))
         for row in range(batch_size):
             new_id = f'{prefix}{base_id + icnt}'
             new_doc = process_message(summary_template, new_id)
@@ -199,9 +207,10 @@ def worker_message_generate(proc_num, stream):
     bb.logit("#-------- COMPLETE -------------#")
     loader = None
 
+
 def process_message(doc_template, new_id):
     cur_doc = bb.read_json(doc_template)
-    age = random.randint(0,6)
+    age = random.randint(0, 6)
     yr = 0
     month = 9
     month = month - age
@@ -209,25 +218,28 @@ def process_message(doc_template, new_id):
         month = 12 + month
         yr = 1
     year = 2022 - yr
-    day = random.randint(1,28)
-    msgdt = datetime.datetime(year,month,day, 10, 45)
+    day = random.randint(1, 28)
+    msgdt = datetime.datetime(year, month, day, 10, 45)
     cur_doc["id"] = new_id
     cur_doc["cmnctn_identifier"] = f'{new_id}_{cur_doc["cmnctn_identifier"]}'
     cur_doc["ext_taxonomy_identifier"] = f'{new_id}_{cur_doc["ext_taxonomy_identifier"]}'
     cur_doc["taxonomy_identifier"] = f'{new_id}_{cur_doc["taxonomy_identifier"]}'
     cur_doc["cmnctn_activity_dts"] = msgdt.strftime("%Y-%m-%dT%H:%M:%S")
-    cur_doc["cmnctn_last_updated_dt"] = (msgdt + datetime.timedelta(hours=4)).strftime("%Y-%m-%dT%H:%M:%S")
+    cur_doc["cmnctn_last_updated_dt"] = (
+        msgdt + datetime.timedelta(hours=4)).strftime("%Y-%m-%dT%H:%M:%S")
     cur_doc["load_day"] = day
     cur_doc["load_month"] = month
     cur_doc["load_year"] = year
     cur_doc["campaign_name"] = fake.sentence()
     cur_doc["taxonomy_cmnctn_content_topic"] = fake.bs()
-    cur_doc["taxonomy_portfolio"] = random.choice(["Behavior Change/Next Best Action","Marketing Inquiry","Survey","Post-call quality check"])
+    cur_doc["taxonomy_portfolio"] = random.choice(
+        ["Behavior Change/Next Best Action", "Marketing Inquiry", "Survey", "Post-call quality check"])
     return(cur_doc)
 
 # --------------------------------------------------------- #
 #  Pub Sub Subscription
 # --------------------------------------------------------- #
+
 
 def message_subscription():
     # read settings and echo back
@@ -251,6 +263,7 @@ def message_subscription():
     # for i in jobs:
     #     i.join()
 
+
 def worker_message_subscribe(num_iterations):
     '''
         read from pub/sub
@@ -259,7 +272,7 @@ def worker_message_subscribe(num_iterations):
 
         single-instance now
         gke - scale up
-    
+
     '''
     #  Send a copy of the claim with one or two field changes
     #  Add an updateDate and sequencenumber, updateName = TCD-1
@@ -280,9 +293,9 @@ def worker_message_subscribe(num_iterations):
     subscription = settings["gcp"]["pub_sub_subscription"]
     timeout = settings["gcp"]["pub_sub_timeout"]
     bb.logit(f'Subscriber set in {project} for topic: {subscription}')
-    g_loader = DbLoader({"settings" : settings})
+    g_loader = DbLoader({"settings": settings})
     icnt = 0
-    
+
     start_time = datetime.datetime.now()
     feed = False
     keep_going = True
@@ -290,7 +303,8 @@ def worker_message_subscribe(num_iterations):
     subscriber = pubsub_v1.SubscriberClient()
     subscription_path = subscriber.subscription_path(project, subscription)
     bb.logit(f"Listening for messages on {subscription_path}..\n")
-    streaming_pull_future = subscriber.subscribe(subscription_path, callback=process_payload)
+    streaming_pull_future = subscriber.subscribe(
+        subscription_path, callback=process_payload)
     base_counter = settings["base_counter"]
     batch_size = settings["batch_size"]
     while keep_going:
@@ -299,7 +313,7 @@ def worker_message_subscribe(num_iterations):
         with subscriber:
             try:
                 # When `timeout` is not set, result() will block indefinitely,
-                # unless an exception is encountered first.                
+                # unless an exception is encountered first.
                 streaming_pull_future.result(timeout=timeout)
             except TimeoutError:
                 streaming_pull_future.cancel()
@@ -309,26 +323,30 @@ def worker_message_subscribe(num_iterations):
     time_diff = (end_time - start_time)
     execution_time = time_diff.total_seconds()
     #file_log(f"{cur_process.name} - Bulk Load took {execution_time} seconds")
-    bb.logit(f"{cur_process.name} - Bulk Load took {execution_time} seconds")   
+    bb.logit(f"{cur_process.name} - Bulk Load took {execution_time} seconds")
     bb.logit("#-------- COMPLETE -------------#")
 
-# callback function for procesrsing consumed payloads 
+# callback function for procesrsing consumed payloads
 # prints recieved payload
+
+
 def process_payload(message):
     global icnt
     new_doc = json.loads((message.data).decode())
     if "_id" in new_doc:
         new_doc["old_id"] = new_doc["_id"]
-        new_doc.pop("_id", None)    
+        new_doc.pop("_id", None)
     new_doc["_id"] = new_doc["taxonomy_identifier"]
     bb.logit(f'Message[{icnt}] {new_doc["taxonomy_identifier"]}.')
     g_loader.add(new_doc)
     icnt += 1
-    message.ack()    
+    message.ack()
 
 #----------------------------------------------------------------------#
 #   Reporting
 #----------------------------------------------------------------------#
+
+
 def claims_reports():
     #  Take an array of claims, give the timeline of changes
     #  Add an updateDate and sequencenumber, updateName = TCD-1
@@ -344,13 +362,15 @@ def claims_reports():
     elif report_type == "claim_details":
         claim_detail()
     else:
-        print(f"No report called {report_type} choices: claim_history, claim_details")
+        print(
+            f"No report called {report_type} choices: claim_history, claim_details")
 
 #----------------------------------------------------------------------#
 #   Utility Routines
 #----------------------------------------------------------------------#
 
-def client_connection(type = "uri", details = {}):
+
+def client_connection(type="uri", details={}):
     mdb_conn = settings[type]
     username = settings["username"]
     password = settings["password"]
@@ -360,16 +380,19 @@ def client_connection(type = "uri", details = {}):
     mdb_conn = mdb_conn.replace("//", f'//{username}:{password}@')
     bb.logit(f'Connecting: {mdb_conn}')
     if "readPreference" in details:
-        client = MongoClient(mdb_conn, readPreference=details["readPreference"]) #&w=majority
+        client = MongoClient(
+            mdb_conn, readPreference=details["readPreference"])  # &w=majority
     else:
         client = MongoClient(mdb_conn)
     return client
+
 
 def increment_version(old_ver):
     parts = old_ver.split(".")
     return(f'{parts[0]}.{int(parts[1]) + 1}')
 
-def check_file(type = "delete"):
+
+def check_file(type="delete"):
     #  file loader.ctl
     ctl_file = "loader.ctl"
     result = True
@@ -379,6 +402,7 @@ def check_file(type = "delete"):
             result = False
     return(result)
 
+
 def file_log(msg):
     if not "file" in ARGS:
         return("goody")
@@ -387,6 +411,7 @@ def file_log(msg):
     stamp = f"{cur_date}|I> "
     with open(ctl_file, 'a') as lgr:
         lgr.write(f'{stamp}{msg}\n')
+
 
 def bulk_writer(collection, bulk_arr):
     try:
@@ -407,7 +432,7 @@ if __name__ == "__main__":
     settings = bb.read_json(settings_file)
     CUR_PATH = os.path.dirname(os.path.realpath(__file__))
     base_counter = settings["base_counter"]
-    IDGEN = Id_generator({"seed" : base_counter})
+    IDGEN = Id_generator({"seed": base_counter})
     MASTER_CLAIMS = []
     if "wait" in ARGS:
         interval = int(ARGS["wait"])
@@ -422,27 +447,26 @@ if __name__ == "__main__":
         worker_load()
     elif ARGS["action"] == "load_updates":
         worker_activity_update_new(1)
+
     elif ARGS["action"] == "subscribe":
         message_subscription()
     elif ARGS["action"] == "publish" and ARGS["stream"] == "kafka":
-        # match ARGS["topic_id"]:
-        #     case "topic1":
-        #         # message_publisher("kafka", "topic1")
-        #         print(1)
-        #     case "topic2":
-        #         # message_publisher("kafka", "topic2")
-        #         print(2)
-        #     case _:
-        #         # message_publisher("kafka")p
-        #         print("Topic selected is not a valid option")
-        message_publisher("kafka")
+        if len(ARGS) == 3:
+            bb.logit(f'Loading summary documents into topic: kafka-topic')
+            message_publisher("kafka", ["kafka-topic"])
+        elif len(ARGS) == 4:
+            topics = ARGS["topics"].split(',')
+            bb.logit(f'Loading summary documents into topics:' + topics[0] +"and" + topics[1])
+            message_publisher("kafka", topics)
+
+
     elif ARGS["action"] == "publish" and ARGS["stream"] == "pubsub":
         message_publisher("pubsub")
     elif ARGS["action"] == "reports":
         claims_reports()
     else:
         print(f'{ARGS["action"]} not found')
-    #conn.close()
+    # conn.close()
 
 '''
 #---- Data Load ---------------------#
---
+++ comm_tracker/test_cases/1.sh
@@ -0,0 +1,2 @@
+cd ..
+python3 commtracker.py action=publish stream=kafka topics=kafka-topic,kafka-topic-2
