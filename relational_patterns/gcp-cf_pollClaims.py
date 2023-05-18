064b5285ddea3b3f753b86ee0cbea1d3391cfb82
Brady Byrd <brady.byrd@mongodb.com>
2023-05-18 13:50:44 -0400

    GCP function, add modified_at

--- relational_patterns/gcp-cf_pollClaims.py
+++ relational_patterns/gcp-cf_pollClaims.py
@@ -28,7 +28,7 @@ def claim_polling_trigger(request):
         return Response({'message': 'Authorization token failed'}, status=403, mimetype='application/json')
     
     last_check = '2023-05-16 14:30:21'
-    warehouse_db = "bradybyrd-poc.claims_warehouse.Member"
+    warehouse_db = "bradybyrd-poc.claims_warehouse.Provider"
     mdb_cred = "mongodb+srv://main_admin:bugsyBoo@claims-demo.vmwqj.mongodb.net"
     mdbconn = MongoClient(mdb_cred)
     db = mdbconn["claim_demo"]
@@ -37,6 +37,7 @@ def claim_polling_trigger(request):
     start_check_at = last_checked_at
     last_check = last_checked_at.strftime("%Y-%m-%d %H:%M:%S")
     tot_processed = 0
+    new_recs = []
     # Timer will trigger every 2 minutes, for 10 sec operation run 12 times
     for iter in range(3):
         query =   f"""
@@ -53,6 +54,8 @@ def claim_polling_trigger(request):
         print("#---------------- RESULTS ---------------#")
         print(f"  run: {last_check}")
         for item in results:
+            item["doc_type"] = "provider_change"
+            new_recs.append(item)
             ids.append(item["member_id"])
             tot_processed += 1
         answer = ",".join(ids)
@@ -60,6 +63,7 @@ def claim_polling_trigger(request):
         time.sleep(10)
         last_checked_at = last_checked_at + datetime.timedelta(seconds=10)
         last_check = last_checked_at.strftime("%Y-%m-%d %H:%M:%S")
+        db["change_activity"].insert_many(new_recs)
     db["preferences"].update_one({"doc_type" : "last_check"},{"$set" : {"checked_at" : last_checked_at}})
     db["activity_log"].insert_one({"activity" : "data polling", "checked_at": orig_checked_at, "processed" : tot_processed})
     return Response({'message': 'successfully connected'}, status=200, mimetype='application/json')
