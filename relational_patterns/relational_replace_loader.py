e50388082b973af0e9366d85de6422bb0d15a4df
Brady Byrd <brady.byrd@mongodb.com>
2023-08-21 13:31:39 -0400

    allow all caps field names

--- relational_patterns/relational_replace_loader.py
+++ relational_patterns/relational_replace_loader.py
@@ -456,7 +456,7 @@ def update_birthdate():
 #   CSV Loader Routines
 #----------------------------------------------------------------------#
 #stripProp = lambda str: re.sub(r'\s+', '', (str[0].lower() + str[1:].strip('()')))
-def strip_prop(str):
+def stripProp(str):
     if str[0].isUpper() and str[1].isLower():
         ans = str[0].lower() + str[1:].strip('()')
     ans = re.sub(r'\s+', '', ans)
